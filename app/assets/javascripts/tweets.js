33a27a9347b57501b42ff6c8f988533a010283df
Situ William <situ_william@yahoo.com>
2018-01-04 18:08:12 -0500

    added ajax requests when tweeting

--- app/assets/javascripts/tweets.js
+++ app/assets/javascripts/tweets.js
@@ -1,2 +1,19 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+document.addEventListener('DOMContentLoaded',function(){
+	var tweet = document.querySelector('#new_tweet')
+	var tweetList = document.querySelector('.tweets')
+	tweet.addEventListener('submit',function(e){
+		e.preventDefault()
+		$.ajax({
+			url: tweet.getAttribute('action'),
+			method: tweet.getAttribute('method'),
+			dataType: 'html',
+			data: $(tweet).serialize()	
+		}).done(function(response){
+
+			tweetList.insertAdjacentHTML('afterbegin',response)
+
+		})
+	})
+})
